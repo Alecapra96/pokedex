bb4fbe7ed33e775e41239f181f95b08077ca3f95
Alejandro <73189430+Alecapra96@users.noreply.github.com>
2021-07-26 17:17:56 -0300

    Tomo otro enfoque

--- src/pokedex.js
+++ src/pokedex.js
@@ -1,28 +1,54 @@
+// let $pokemonList = document.querySelector("#pokemon-list-div");
+// let count = 0;
+
+// function addDiv () {
+
+//     let createDiv = document.createElement("div");
+//     createDiv.setAttribute("id", `pokemon-${count}`);
+//     createDiv.setAttribute("class", "pokemonDivs");
+//     $pokemonList.appendChild(createDiv);
+//     createDiv.textContent = "contenido";
+// }
+// function completeDiv(element){
+//     let name = element.name;
+//     let url = element.url;
+//     let $selectedDiv = document.querySelector(`#pokemon-${count}`)
+//     $selectedDiv.textContent = name;
+//     count++
+// }
+// function showInfo(){
+//     let $pokemonDivs =document.querySelectorAll(".pokemonDivs")
+//     $pokemonDivs.forEach(function(pokemon){
+//         pokemon.onclick=function(){
+//             fetchShowInfo();
+//         }
+//     })
+// }
+// function fetchShowInfo(){
+//     fetch(`https://pokeapi.co/api/v2/pokemon/1`)
+//     .then(response => response.json())
+//     .then(data => {
+//        data.results.forEach(element => {
+//             console.log(element)
+//             })
+//        });
+
+// }
+// fetch('https://pokeapi.co/api/v2/pokemon')
+//   .then(response => response.json())
+//   .then(data => {
+//     data.results.forEach(element => {
+//         addDiv();
+//         completeDiv(element);
+//         showInfo();
+//       })
+//     });
+
+
 let $pokemonList = document.querySelector("#pokemon-list-div");
 let count = 0;
-
-function addDiv () {
-    count++
-    // let idDiv =  que se sume 1 
-    let createDiv = document.createElement("div");
-    createDiv.setAttribute("id", `pokemon-${count}`);
-    createDiv.setAttribute("class", "tags");
-    $pokemonList.appendChild(createDiv);
-    createDiv.textContent = "contenido";
-}
-
-fetch('https://pokeapi.co/api/v2/pokemon')
-  .then(response => response.json())
-  .then(data => {
-    let number = 0;
-    data.results.forEach(element => {
-        addDiv();
-        console.log(element.name)
-        // Object.keys(data.number).forEach(element => {
-        //     console.log(element)
-        //      number++;
-        //  })
-      })
-    });
+Creo un div grande que sea la "pagina" donde se muestran los pokemones, cuando cambio de pagina, se wipean los datos de dentro y hago una fetch a la proxima pagina de la api
+Creo 20 casillas para los pokemones ,dentro de este div , sumandole un numero a el ultimo digito de el link de la api en cada cuadro 
+muestro la informacion de los pokemones en cada cuadro cuando le hagan click.
 
 
